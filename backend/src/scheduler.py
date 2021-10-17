ffe8a29bb6b4d880715bc6db399007186d62f03b
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-10-17 00:12:37 -0600

    Fixing the scheduler

--- backend/src/scheduler.py
+++ backend/src/scheduler.py
@@ -11,16 +11,14 @@ if __name__ == "__main__":
     rows = db.get_events_api_key()
 
     if len(rows) > 0:
+        print("Found event to execute!")
+
         (id, api_key) = rows.pop()
-
-        print("Event with id:", id, "and api_key:", api_key)
-
         db.delete_events(id)
 
         print("Deleted stored event!")
 
         start_sunrise(api_key)  
-
         print("Finished executing the scheduler!")  
     else:
         print("No events to execute!")
