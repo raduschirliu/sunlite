e8a14564186b3ae8d8db57297108e9956188cf1e
raduschirliu <radu.schirliu@gmail.com>
2021-10-16 19:10:17 -0600

    chore: add logging

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -8,15 +8,18 @@ JWT_AUDIENCE = os.getenv('JWT_AUDIENCE')
 # Check if the request header includes a valid JWT
 def verify_jwt():
     if not hasattr(request.headers, 'Authorization'):
+        print('No authorization')
         return False
 
     authorization = request.headers.get('Authorization')
     parts = authorization.split(' ')
     
     if len(parts) != 2:
+        print('Invalid parts')
         return False
     
     if parts[0] != 'Bearer':
+        print('No bearer')
         return False
     
     try:
@@ -24,4 +27,5 @@ def verify_jwt():
         decoded = jwt.decode(parts[1], JWT_SECRET, audience=audience, algorithms=['HS256'])
         return decoded
     except Exception:
+        print('Invalid JWT')
         return False
